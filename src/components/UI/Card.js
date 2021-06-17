ad7c4629e4bd17101180b29f487513b2e4562358
Roberto Carlos <robertocarlosmedina.dev@gmail.com>
2021-06-17 12:19:46 -0100

    Delete src directory

--- src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-// para colocar todos as componentes de um conteiner do nosso app
-import './card.css'
-
-function Card(props){
-    const classes = ' card'+ props.className;
-    return <div className={classes}>{props.children}</div>
-}
-
-export default Card;
